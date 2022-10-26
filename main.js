bebd3b5df1380e1a76178491d38cba02e1541e56
Ahmad Nagy <ahmadnagy55@gmail.com>
2022-10-26 16:57:08 +0200

    show and hide search page with js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
+// hide and show back to top button
 document.body.onscroll = function ()
 {
     let toTop = document.getElementById('back-to-top');
-    toTop.style.display = "none";
-    if(window.scrollY >= 200)
+    if(window.scrollY >= 100)
     {
     toTop.style.display = 'block';
     }else
@@ -10,3 +10,14 @@ document.body.onscroll = function ()
     toTop.style.display = "none";
     }
 };
+// show and hide search page 
+
+    let searchIcon = document.getElementById('search'),
+    hiddenPage= document.getElementById('hidden-page');
+searchIcon.onclick = function () {
+    hiddenPage.style.display = 'block';
+}
+let closeIcon = document.getElementById("icon-close");
+closeIcon.onclick = function () {
+    hiddenPage.style.display = 'none';
+}
